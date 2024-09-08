64b731620a1c180d7fd45f9ab23faec9cabd9f05
Anas <158191488+Mdas109@users.noreply.github.com>
2024-09-08 21:12:17 +0530

    Add files via upload

---
+++ SS1T.ts
@@ -0,0 +1,7 @@
+#EXTM3U
+#EXT-X-TARGETDURATION:
+#EXT-X-VERSION:3
+#EXTINF:0.01, BANDWIDTH=3000000, FRAME-RATE=60.0, AUDIO="5.1", CODECS="avc1.640028,ec-3.1.1,mp4a.40.5",
+http://premiumdedi.one/live/Jubilant7Relative/xmb1HEJBsY/451473.ts
+#EXTINF:0.01, BANDWIDTH=3000000, FRAME-RATE=60.0, AUDIO="5.1", CODECS="avc1.640028,ec-3.1.1,mp4a.40.5",
+http://premiumdedi.one/live/Jubilant7Relative/xmb1HEJBsY/451473.ts
